64ce47d0cadef9ea2afcfe59a79a12090c5d812d
helen coutinho <115495648+helenszc@users.noreply.github.com>
2022-10-31 13:57:48 -0300

    oficina
    adição de classe: carro e pneu

---
+++ Oficina/Oficina/Program.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+
+namespace Oficina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pneu pneu = new Pneu (10, "michelin");
+            Carro corcel = new Carro("Chevrolet","2" );
+            corcel.LigarDesligar();
+            corcel.Abastecer(150);
+            corcel.Acelerar(5);
+            corcel.Frear(30);
+            corcel.Acelerar(100);
+            corcel.Frear(50);
+            
+            corcel.Exibir();
+
+
+        }
+    }
+}
